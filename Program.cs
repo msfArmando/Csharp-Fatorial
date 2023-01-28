b1b51fb93c50970c121ef869dd7e2ac934f071bc
Armando Filho <82972333+msfArmando@users.noreply.github.com>
2023-01-28 11:44:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FATORIAL");
+            Console.WriteLine("--------");
+
+            Console.WriteLine("De qual número você quer saber o fatorial? ");
+            int fatorial = int.Parse(Console.ReadLine());
+
+            int num = 1;
+            int res = 0;
+            for(int n = 1; n <= fatorial; n++)
+            {
+                res = num * n;
+                num = res;
+            }
+            Console.WriteLine("O fatorial de " + fatorial + " é igual a: " + res);
+            Console.ReadLine();
+        }
+    }
+}
